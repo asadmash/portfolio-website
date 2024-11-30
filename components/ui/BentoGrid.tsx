660ab7f33bc9ddba85f4d4753f06cbc5f00cb482
asadmash <mdasad.mash@gmail.com>
2024-11-30 15:27:06 +0600

    card background gradient added

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -47,8 +47,9 @@ export const BentoGridItem = ({
         className
       )}
       style={{
-        background: "rgb(4, 7, 29)",
-        backgroundColor: "gradient...",
+        background: "rgb(4,7,29)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
       <div className="group-hover/bento:translate-x-2 transition duration-200">
