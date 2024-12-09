0d2eedf06171d34d445334b6dd827f69e9ec8758
asadmash <mdasad.mash@gmail.com>
2024-12-09 16:02:12 +0600

    navigation section implemented

--- components/ui/RecentsProjects.tsx
+++ components/ui/RecentsProjects.tsx
@@ -5,7 +5,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 const RecentsProjects = () => {
   return (
-    <div className="py-20">
+    <div className="py-20" id="projects">
       <h1 className="heading">
         A small collection of{" "}
         <span className="text-purple">recent projects</span>
