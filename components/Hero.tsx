fb74c277f8c6de49fda247e5855f9f2e7ed64343
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-10 00:32:31 +0600

    added bg beam & optimized hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,16 @@
 // "use client";
 import React from "react";
-import { Spotlight } from "./ui/Spotlight";
+// import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import MagicButton from "./ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa6";
+import {BackgroundBeamsWithCollision} from "./ui/Background-beams-with-collision";
 // import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
     <>
-      <div className="pb-20 pt-36">
+      {/* <div className="pb-20 pt-36">
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
           fill="white"
@@ -19,30 +20,35 @@ const Hero = () => {
           fill="purple"
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[80vw]" fill="blue" />
-      </div>
-{/* grid background */}
-      <div className="h-screen w-full dark:bg-[#15181e] bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] absolute flex items-center justify-center top-0 left-0">
-        {/* Radial gradient for the container to give a faded look */}
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-[#15181e] bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-      </div>
-      {/* heading text */}
-      <div className="flex justify-center relative my-20 z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic web magic with react.js
-          </h2>
-          <TextGenerateEffect
-            className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seamless User Experiences"
-          />
-          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi, I am asadMash, a react js developer.
-          </p>
-          {/* magic button */}
-          <a href="#projects">
-            <MagicButton title="Show Your Work" icon={<FaLocationArrow/>} position="right"/>
-          </a>
+      </div> */}
+       <div className="h-screen w-full flex items-center justify-center">
+     <BackgroundBeamsWithCollision className="h-screen w-full">
+        {/* grid background */}
+       
+          {/* heading text */}
+         <div className="flex justify-center relative my-20 z-10">
+          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+              Dynamic web magic with react.js
+            </h2>
+            <TextGenerateEffect
+              className="text-center text-[40px] md:text-5xl lg:text-6xl"
+              words="Transforming Concepts into Seamless User Experiences"
+            />
+            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+              Hi, I am asadMash, a react js developer.
+            </p>
+            {/* magic button */}
+            <a href="#projects">
+              <MagicButton
+                title="Show Your Work"
+                icon={<FaLocationArrow />}
+                position="right"
+              />
+            </a>
+          </div>
         </div>
+     </BackgroundBeamsWithCollision>
       </div>
     </>
   );
