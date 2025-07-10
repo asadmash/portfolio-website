2e828a03d3b8ee36b21d70723843c92b3df41758
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-10 14:27:18 +0600

    removed company logo

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -6,9 +6,9 @@ import { companies, testimonials } from "@/data";
 const Clients = () => {
   return (
     <div className="py-20" id="testimonials">
-      <h1 className="heading">
+      <h1 className="heading mb-10">
         Kind words from
-        <span className="text-[#33beba]"> satisfiend clients</span>
+        <span className="text-[#33beba] "> satisfiend clients</span>
       </h1>
       <div className="flex flex-col items-center max-lg:mt-10">
         {/* Infinity moving card component */}
@@ -19,14 +19,14 @@ const Clients = () => {
        
         />
         {/* companies */}
-        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
+        {/* <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
           {companies.map(({ id, img, name, nameImg }) => (
             <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
               <img src={img} alt={name} className="md:w-10 w-5" />
               <img src={nameImg} alt={name} className="md:w-24 w-20" />
             </div>
           ))}
-        </div>
+        </div> */}
       </div>
     </div>
   );
