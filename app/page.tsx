eefc437338cb49b12824761c39dbd1c17713429f
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-13 22:03:38 +0600

    added svg

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 // "use client";
-import {Approch} from "@/components/Approch";
-import {Clients} from "@/components/Clients";
-import {Experience} from "@/components/Experience";
+import { Approch } from "@/components/Approch";
+import { Clients } from "@/components/Clients";
+import { Experience } from "@/components/Experience";
 import { FloatingDockComp } from "@/components/FloatingDockComp";
-import {Footer} from "@/components/Footer";
+import { Footer } from "@/components/Footer";
 // import Grid from "@/components/Grid";
-import {Hero} from "@/components/Hero";
+import { Hero } from "@/components/Hero";
 // import { FloatingNav } from "@/components/ui/FoatingNav";
-import {RecentsProjects} from "@/components/RecentsProjects";
+import { RecentsProjects } from "@/components/RecentsProjects";
 import { navItems } from "@/data";
 // import { FaHome } from "react-icons/fa";
 import { SpeedInsights } from "@vercel/speed-insights/next";
@@ -16,24 +16,25 @@ import Aboutme from "@/components/Aboutme";
 
 const Home = () => {
   return (
-    <main className="relative bg-[#15181e] flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-2 ">
-        <SpeedInsights/>
-        <Analytics/>
-               <Hero/>
-      <div className="max-w-7xl w-full">
-      <FloatingDockComp/>
-        {/* <FloatingNav navItems={navItems}/> */}
- 
-      {/* <Grid/> */}
+    <main className="relative w-screen bg-[#15181e] flex justify-center items-center flex-col overflow-hidden ">
+      {/* mx-auto sm:px-10 px-2 */}
+      <SpeedInsights />
+      <Analytics />
+      <Hero />
       <Aboutme />
-      <RecentsProjects/>
-      <Clients/>
-      <Experience/>
-      <Approch/>
-      <Footer/>
+      <div className="max-w-7xl w-full">
+        <FloatingDockComp />
+        {/* <FloatingNav navItems={navItems}/> */}
+
+        {/* <Grid/> */}
+        <RecentsProjects />
+        <Clients />
+        <Experience />
+        <Approch />
+        <Footer />
       </div>
     </main>
   );
-}
+};
 
 export default Home;
